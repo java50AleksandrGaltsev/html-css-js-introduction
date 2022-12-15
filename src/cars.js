561931166aea84ffa03204556752f99b3e006266
Alexandergaltcev <sash-galce@yandex.ru>
2022-12-15 21:59:36 +0200

    adds HW#9

--- src/cars.js
+++ src/cars.js
@@ -1,15 +1,24 @@
- const detailsImage = document.querySelector(".details-image");
- const detailsTitle = document.querySelector (".details-title");
- const anchorElements = document.querySelectorAll (".thumbnails-anchor");
- function setDetails(anchor) {
-    const dataImage = anchor.getAttribute("data-details-image");
-    detailsImage.src = dataImage;
-    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
-
- }
+const detailsImage = document.querySelector(".details-image");
+const detailsTitle = document.querySelector (".details-title");
+const anchorElements = document.querySelectorAll (".thumbnails-anchor");
+const mainElement = document.querySelector(".main-class");
+const hideButtonElement = document.querySelector ("#hide-button");
+const HIDDEN = "hidden";
+function showDetails () {
+mainElement.classList.remove(HIDDEN);
+}
+function hideDetails () {
+mainElement.classList.add(HIDDEN);
+}
+function setDetails (anchor) {
+const dataImage = anchor.getAttribute("data-details-image");
+detailsImage.src = dataImage;
+showDetails();
+detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+}
  for (let i = 0; i < anchorElements.length; i++) {
-    anchorElements[i].addEventListener("click", function (){
+    anchorElements[i].addEventListener ("click", function () {
         setDetails(anchorElements[i]);
     })
  }
- 
+ hideButtonElement.addEventListener ("click", hideDetails);
