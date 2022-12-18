9afa75ce5f515aade1e8b36f0b3763d7658e535d
Alexandergaltcev <sash-galce@yandex.ru>
2022-12-18 21:48:22 +0200

    adds new animation

--- src/cars.js
+++ src/cars.js
@@ -1,20 +1,26 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector (".details-title");
+const detailsContainer = document.querySelector (".details-conteiner");
 const anchorElements = document.querySelectorAll (".thumbnails-anchor");
-const mainElement = document.querySelector(".main-class");
+const mainElement = document.querySelector (".main-class");
 const hideButtonElement = document.querySelector ("#hide-button");
+const IS_POINT = "is-point";
 const HIDDEN = "hidden";
 function showDetails () {
-mainElement.classList.remove(HIDDEN);
-}
-function hideDetails () {
-mainElement.classList.add(HIDDEN);
+    mainElement.classList.remove (HIDDEN);
+detailsContainer.classList.add(IS_POINT);
+setTimeout(function() {
+    detailsContainer.classList.remove(IS_POINT);
+})
 }
 function setDetails (anchor) {
 const dataImage = anchor.getAttribute("data-details-image");
 detailsImage.src = dataImage;
 showDetails();
 detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+}
+function hideDetails () {
+    mainElement.classList.add (HIDDEN);
 }
  for (let i = 0; i < anchorElements.length; i++) {
     anchorElements[i].addEventListener ("click", function () {
