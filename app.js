a3a4c9d9ffe93768cba19bd3b2103f84fb1f2b68
Alexandrgaltcev <sash-galce@yandex.ru>
2023-01-14 20:13:19 +0300

    adds HW# 15

--- app.js
+++ app.js
@@ -1,78 +1,109 @@
-const array = [10, 20, 30, -10, 11, 100];
-//array.splice(2, 2);
- //const index = array.indexOf(30);
-//removing all numbers from 30
-// const res = array.splice(index, 10); //udalyaet is massive element po index, esli xotim poluchit udalenni element to stavim res
-//array.splice(index, undefined, -1000)
-//insert
-//array.splice(index, 0, -10, -20);
-//replace
-// array.splice(index, 2, 20);
-// const ar1 = [40,200];
-// array.splice(index,1, ...ar1);
-// const numbersPerPage = 2;
-// const page = 2;
-// const firstIndex = (page -1) * numbersPerPage;
-// const pageNumbers = array.slice(firstIndex, 
-//     firstIndex + numbersPerPage)
-// const ar1 = array.map(multiply2);
-// function multiply2(number) {
-//     return number * 2;
-// }
-// const ar2 = array.map(function (number, index){
-//     return index + 1 + '. ' + number;
-// });
-// const ar3 = array.map (function(number) {
-// return `<li>${number}</li>`
-// })
-// const ulElement = `<ul>${ar3.join('')}</ul>`;
-
-function coloringString (str1, str2) {
-    //const ar1 = Array.from(str1);
-    const arStr2 = Array.from(str2);
-    const res = arStr2.map(function (symbol, index) {
-        let color;
-      if(symbol === str1[index]) {
-        color = "green"
-      } else {
-        color = str1.includes(symbol) ? "yellow" : "red";
-      }
-      return color;
+const array = [123, 9, 28, 3, 44];
+array.sort(function(e1, e2) {
+    return e1-e2;
+    // return e2 - e1
+   // return e1.toString().length -e2.toString().length;
+//    let res = e1.toString().length - e2.toString().length
+//    if(!res) {
+//     res = e1 - e2;
+//    }
+//   return res;
+});
+console.log(`result of sorting [123, 9, 28, 3, 44] is ${array}`)
+//HW #15
+//********************************************* */
+//task sort1
+function evenOddSort (array) {
+   return array.sort(function(e1, e2) {
+       return e1 % 2 == 0 ? -1 : 1;
+       
     })
+    
+    //sort arrray of numbers in the  order even numbers go before the odd ones
+    // example input array[20, -10, 333, 1000, 552, 7, -7] => [20, -10, 1000, 552, 333, 7, -7]
+}
+//task sort2
+function oddEvenSort (array) {
+    return array.sort(function(e1, e2) {
+        return e1 % 2 == 0 ? 1 : -1;
+})
+}
+//task sort 3
+function evenAscDesc(array) {
+    return array.sort(function(e1, e2){
+        let res = Math.abs(e1 % 2) - Math.abs(e2 % 2);
+        if (res === 0) {
+            res = e1 % 2 == 0 ? e1 - e2 : e2 - e1;
+        }
+        return res;
+    })
+}
+//****************************************** */
+
+//reduce
+//find sum of the numbers in an array
+function sum (array) {
+    const res = array.reduce(function(res, cur) {
+        return res + cur;
+    });
     return res;
+}
+console.log(`sum([1,2,3,4,5]) returns ${sum([1,2,3,4,5])}`); 
+
+[1, 2, 3, 4].reduce(function (x, y) {
+    console.log(x,y);
+    return x + y; 
+}); // 1- function call x = 1, y = 2; 2 function call x =  undefind, y = 3, 3 func call x = undefind, y = 4
+
+console.log([[0, 1], [2, 3]].reduce (
+    (acc, cur) => {
+        return acc.concat(cur);
+    }, 
+    [1, 2]
+));
+// 1) acc - [1, 2], cur - [0, 1]
+//2) acc - [1, 2, 0, 1], cur - [2, 3]
+
+//HW#15 
+// task reduce 1
+ function getMin (array) {
+   return array.reduce(function (min, cur){
+    return min > cur ? cur : min;
+    
+   })
    
-
-}
-//getting array of numbers in a given range. Example as follows
-// const min = 5;
-// const max = 15;
-// const ar4 = array.filter(function (number) {
-//     return number >= min && number <= max;
-// })
-//getting new array of the elements of the source array with even index
-// const ar5 = array.filter(function (__, index) {
-//     return index % 2 === 0;
-// })
-//HW#14 (2) 
-function getNumbersWithDigitsAmount (digitsAmount, array) {
-
-    const res = array.filter (function (number) {
-        const nDigits = getNumberDigits (number);
-        return nDigits === digitsAmount;
-        // if (number < 0) {
-        //     number = -number;
-        // }
-        // let ind = number.toString();
-        // if (ind.length == digitsAmount) {
-        //     return number;
-        // }
-    })
-return res;
-}
-function getNumberDigits (number) {
-    number = Math.abs(Math.trunc(number));
-    const res = number.toString().length;
+  // return res;
+ }
+ //task reduce 2
+ function getMax (array) {
+     const res = array.reduce (function(res, index) {
+        return res > index ? res = res : res = index;
+     })
+     return res;
+ }
+ //task reduce 3
+ function getAverage (array) {
+   const sum1 = sum(array);
+   return sum1/array.length;
+ }
+ //task reduce 4
+ function getMinMaxAvg (array) {
+    const res = array.reduce(function (ar, cur){
+        if (cur < ar[0]) {
+            ar[0] = cur;
+        } else if (cur > ar[1]) {
+            ar[1] = cur;
+        }
+        ar[2] += cur;
+        return ar;
+    }, [array[0], array[0], 0]);
+    res[2] /= array.length;
     return res;
-}
-console.log (`coloringString ("pappy", "apple") returns -- ${coloringString ("pappy", "apple").join(' ')}` );
-console.log(`getNumbersWithDigitsAmount (3, [1, 100, -100, 25, 1000]) returns  ${getNumbersWithDigitsAmount (3, [1, 100, -100, 25, 1000])}`);
+ }
+ console.log(`evenOddSort ([20, -10, 333, 1000, 552, 7, -7]) => ${evenOddSort ([20, -10, 333, 1000, 552, 7, -7])}`);``
+ console.log(`oddEvenSort([20, -10, 333, 1000, 552, 7, -7]) => ${oddEvenSort([20, -10, 333, 1000, 552, 7, -7])}`);
+ console.log (`evenAscDesc([20, -10, 333, 1000, 552, 7, -7]) => ${evenAscDesc([20, -10, 333, 1000, 552, 7, -7])}`);
+ console.log(`minValue of array [20, -10, 333, 1000, 552, 7, -7] is ${getMin ([20, -10, 333, 1000, 552, 7, -7])}`);
+ console.log (`maxValue of array [20, -10, 333, 1000, 552, 7, -7] is ${getMax ([20, -10, 333, 1000, 552, 7, -7])}`);
+ console.log(`average of array ([20, -10, 333, 1000, 552, 7, -7]) is ${getAverage([20, -10, 333, 1000, 552, 7, -7])} `);
+ console.log (`getMinMaxAvg ([20, -10, 333, 1000, 552, 7, -7]) => ${getMinMaxAvg ([20, -10, 333, 1000, 552, 7, -7])}`);
